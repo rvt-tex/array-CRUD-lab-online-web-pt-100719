faf15051714237e9c1a11d5632989eac22515186
Ruel Tingling <rvtingling@yahoo.com>
2019-10-10 20:00:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["tom", "john", "bob", "arrays!"]
-  #arrays! = array.pop
+  arrays[-1] = array.pop
 end
 
 def remove_element_from_start_of_array(array)
